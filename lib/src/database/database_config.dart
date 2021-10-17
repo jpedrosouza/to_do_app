c0233cc781f0106e29b914bfd3eaa472cf12e8d8
João Pedro Souza <jsouza.1650@gmail.com>
2021-10-17 04:22:52 -0300

    created database config and tables

---
+++ lib/src/database/database_config.dart
@@ -0,0 +1,30 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseConfig {
+  /// Open database, and create database file.
+
+  Future<Database> getDatabase() async {
+    return openDatabase(
+      join(await getDatabasesPath(), 'to_do_app_database.db'),
+      onCreate: (db, version) async {
+        await createTable(db);
+      },
+      onOpen: (db) {},
+      onUpgrade: (db, oldVersion, newVersion) async {},
+      onDowngrade: (db, oldVserion, newVersion) async {},
+      version: 1,
+    );
+  }
+
+  /// Create table in database if not exists.
+
+  Future createTable(Database db) async {
+    await db.execute(
+        'CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, task_name '
+        'TEXT NOT NULL, completed INTEGER NOT NULL, group_id INTEGER);');
+
+    await db.execute('CREATE TABLE IF NOT EXISTS task_groups ('
+        'id INTEGER PRIMARY KEY, name TEXT NOT NULL');
+  }
+}
